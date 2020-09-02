ce47178c668280ccf6d19a53a7d645bbedaf6ff2
Sachin Vidhate <sachin.vidhate@vedirasolutions.com>
2020-09-02 12:33:39 +0530

    changed auto Id generation strategy in datamodels

--- src/main/java/com/springboot/api/domain/User.java
+++ src/main/java/com/springboot/api/domain/User.java
@@ -11,7 +11,7 @@ import javax.persistence.UniqueConstraint;
 		@UniqueConstraint(columnNames = "email") })
 public class User {
 	@Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
     private String name;
     private String city;
